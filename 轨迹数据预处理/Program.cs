ee3ccc26751ae58cbd7b75d6e4fbbee526700455
MagicSong <programsong@hotmail.com>
2017-05-08 21:55:01 +0800

    a little improvements

--- 轨迹数据预处理/Program.cs
+++ 轨迹数据预处理/Program.cs
@@ -18,7 +18,8 @@ namespace 轨迹数据预处理
         {
             //OnceFunction.CentroidToShapefile();
             //CentroidCorrection.CorretionCentroid();
-            Console.WriteLine("Hello World");
+            GetDataOfEachCentoid();
+            //Console.WriteLine("Hello World");
         }
 
         static void GetDataOfEachCentoid()
@@ -26,8 +27,8 @@ namespace 轨迹数据预处理
             Stopwatch sw = new Stopwatch();
             sw.Start();
             Console.WriteLine("开始读取数据");
-            IFeatureSet centroid = FeatureSet.Open(@"D:\OneDrive\2017研究生毕业设计\数据\项目用数据\newCentroid.shp");
-            IFeatureSet LargePoints = FeatureSet.Open(@"D:\OneDrive\2017研究生毕业设计\数据\项目用数据\myFinalPts.shp");
+            IFeatureSet centroid = FeatureSet.Open(@"D:\MagicSong\OneDrive\2017研究生毕业设计\数据\项目用数据\newCentroid.shp");
+            IFeatureSet LargePoints = FeatureSet.Open(@"D:\MagicSong\OneDrive\2017研究生毕业设计\数据\项目用数据\OriginalGauss.shp");
             sw.Stop();
             Console.WriteLine("数据读取完成，耗费时间为{0}s", sw.Elapsed.TotalSeconds);
             sw.Restart();
@@ -39,15 +40,29 @@ namespace 轨迹数据预处理
                 myTree.Insert(new double[] { c.X, c.Y }, item);
             }
             Console.WriteLine("KD树构建完成");
-            LargePoints.DataTable.Columns.Add("Subject", typeof(int));
+            List<int>[] result = new List<int>[centroid.NumRows()];
             int core = 8;
-            int PointsCount = LargePoints.NumRows() / 8;
+            double eachTaskPointsCount = LargePoints.NumRows() / 8.0;
             Parallel.For(0, core, new Action<int>((index) =>
             {
                 //并行任务开始
-
-
+                int start = (int)Math.Ceiling(index * eachTaskPointsCount);
+                int end = index != 7 ? (int)Math.Floor((index + 1) * eachTaskPointsCount) : LargePoints.NumRows() - 1;
+                Console.WriteLine("开始为：{0}，终点为：{1}", start, end);
+                for (int i = start; i <= end; i++)
+                {
+                    var currentFeature = LargePoints.GetFeature(i);
+                    Coordinate c = currentFeature.Coordinates[0];
+                    IFeature f = myTree.Nearest(new double[] { c.X, c.Y }) as IFeature;
+                    if(f.Distance(currentFeature)<300)
+                    {
+                        result[f.Fid].Add(currentFeature.Fid);
+                    }
+                }
             }));
+            sw.Stop();
+            Console.WriteLine("计算结束！还需保存！耗时：{0}",sw.Elapsed.TotalSeconds);
+            Console.ReadKey();
         }
     }
 }

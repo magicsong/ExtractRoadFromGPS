2abd96593a499d8c023a321d2d16b8a5390f7366
MagicSong <625448253@qq.com>
2017-05-09 17:22:01 +0800

    宋雪涛是废物版本1

--- 轨迹数据预处理/Program.cs
+++ 轨迹数据预处理/Program.cs
@@ -19,16 +19,32 @@ namespace 轨迹数据预处理
             //OnceFunction.CentroidToShapefile();
             //CentroidCorrection.CorretionCentroid();
             GetDataOfEachCentoid();
+            //DuplicateShapeElimination();
             //Console.WriteLine("Hello World");
         }
-
+        static void DuplicateShapeElimination()
+        {
+            IFeatureSet centroid = FeatureSet.Open(@"D:\OneDrive\2017研究生毕业设计\数据\项目用数据\newCentroid.shp");
+            HashSet<string> set = new HashSet<string>();
+            List<int> index = new List<int>(5);
+            foreach (var item in centroid.Features)
+            {
+                string name = item.DataRow["name"].ToString();
+                if (!set.Add(name))
+                    index.Add(item.Fid);
+            }
+            centroid.RemoveShapesAt(index);
+            Console.WriteLine("数据删除完毕");
+            centroid.SaveAs("newCentroidNoDup.shp", true);
+            Console.ReadKey();
+        }
         static void GetDataOfEachCentoid()
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
             Console.WriteLine("开始读取数据");
-            IFeatureSet centroid = FeatureSet.Open(@"D:\MagicSong\OneDrive\2017研究生毕业设计\数据\项目用数据\newCentroid.shp");
-            IFeatureSet LargePoints = FeatureSet.Open(@"D:\MagicSong\OneDrive\2017研究生毕业设计\数据\项目用数据\OriginalGauss.shp");
+            IFeatureSet centroid = FeatureSet.Open(@"D:\OneDrive\2017研究生毕业设计\数据\项目用数据\newCentroidNoDup.shp");
+            IFeatureSet LargePoints = FeatureSet.Open(@"D:\OneDrive\2017研究生毕业设计\数据\项目用数据\OriginalGauss.shp");
             sw.Stop();
             Console.WriteLine("数据读取完成，耗费时间为{0}s", sw.Elapsed.TotalSeconds);
             sw.Restart();
@@ -41,6 +57,8 @@ namespace 轨迹数据预处理
             }
             Console.WriteLine("KD树构建完成");
             List<int>[] result = new List<int>[centroid.NumRows()];
+            for (int i = 0; i < result.Length; i++)
+                result[i] = new List<int>();
             int core = 8;
             double eachTaskPointsCount = LargePoints.NumRows() / 8.0;
             Parallel.For(0, core, new Action<int>((index) =>
@@ -54,14 +72,14 @@ namespace 轨迹数据预处理
                     var currentFeature = LargePoints.GetFeature(i);
                     Coordinate c = currentFeature.Coordinates[0];
                     IFeature f = myTree.Nearest(new double[] { c.X, c.Y }) as IFeature;
-                    if(f.Distance(currentFeature)<300)
+                    if (f.Distance(currentFeature) < 300)
                     {
                         result[f.Fid].Add(currentFeature.Fid);
                     }
                 }
             }));
             sw.Stop();
-            Console.WriteLine("计算结束！还需保存！耗时：{0}",sw.Elapsed.TotalSeconds);
+            Console.WriteLine("计算结束！还需保存！耗时：{0}", sw.Elapsed.TotalSeconds);
             Console.ReadKey();
         }
     }
